60d958f43b94bba74dc7d857d1f7004d2e321cfd
haocao1995 <haocao1995@gm.com>
2018-10-19 11:48:30 +0700

    second commit

---
+++ Lab 02/name.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    cout<<"*       *         *         ***********"<<endl;
+    cout<<"*       *        * *        *         *"<<endl;
+    cout<<"*       *       *   *       *         *"<<endl;
+    cout<<"*********      *     *      *         *"<<endl;
+    cout<<"*       *     *********     *         *"<<endl;
+    cout<<"*       *    *         *    *         *"<<endl;
+    cout<<"*       *   *           *   ***********"<<endl;
+    return 0;
+}
+
